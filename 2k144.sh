ccd9b46c360f55c9bb6cc3ab2aa0c5ba93dfa880
Ultimate Script <armandtourland6@gmail.com>
2026-07-04 11:19:57 +0200

    Create 2k144.sh

---
+++ 2k144.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "🔧 Configuration en 2K (1440p) 144Hz..."
+OLD_VIDEO=$(rpm-ostree kargs | tr ' ' '\n' | grep '^video=')
+
+if [ ! -z "$OLD_VIDEO" ]; then
+    sudo rpm-ostree kargs --replace="$OLD_VIDEO=video=2560x1440@144"
+else
+    sudo rpm-ostree kargs --append="video=2560x1440@144"
+fi
+echo "✅ Fait ! Redémarrage dans 5 secondes..."
+sleep 5
+sudo systemctl reboot
